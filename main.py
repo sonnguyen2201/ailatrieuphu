6bac0c9f96d16a8dd7120bf274afe07eb091b526
nguyenvanson2201 <n.vanson.2201@gmail.com>
2020-02-02 16:38:54 +0700

    add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+play = input('Bạn có muốn chơi game không [y/n]:')
+if play == 'y':
+    print('Trò chơi bắt đầu')
+    print('-' * 100)
+    print('Câu hỏi 1: Việt Nam có hình chữ gì:')
+    print('A: Chữ G')
+    print('B: Chữ H')
+    print('C: Chữ O')
+    print('D: Chữ S')
+    user_answer = input('Đáp án bạn chọn là [A/B/C/D]:')
+    if user_answer == 'D':
+        print('Bạn trả lời đúng.')
+    else:
+        print('Bạn trả lời sai. Trò chơi kết thúc!')
+elif play == 'n':
+    print('Trò chơi kết thúc')
+
